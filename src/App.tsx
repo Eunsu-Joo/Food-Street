b297ec483aca1d289b580a9b64788cbe3fe4729d
Eunsu-Joo <holicholicpop@gmail.com>
2022-12-16 17:11:55 +0900

    ◾ queryClient 기본설정 수정
    => App에서 useNavigator를 호출하면 모든 페이지에서 쿼리가 초기화가됨.

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,31 @@
-import React from "react";
-import { Container, GlobalStyles } from "@mui/material";
+import React, { useState } from "react";
+import { Container } from "@mui/material";
 import BottomBar from "./components/_common/bottombar";
 import Header from "./components/_common/header";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import Routes from "./components/_common/routes";
-import useUser from "./hooks/useUser";
+import Loading from "./components/_common/loading";
 
 function App() {
+  //   const navigator = useNavigate(); 이 부분 떄문에 쿼리 초기화됨.
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 1000 * 20,
+        staleTime: 1000 * 60,
         retry: 1,
         cacheTime: 1000 * 60 * 10,
         refetchOnReconnect: false,
-        retryOnMount: false,
-        refetchOnWindowFocus: false,
-      },
-    },
+        retryOnMount: true,
+        refetchOnWindowFocus: false
+      }
+    }
   });
   return (
     <QueryClientProvider client={queryClient}>
-      <Container
-        sx={{ pt: 10, minHeight: "calc(100vh - 56px)", pb: 10 }}
-        maxWidth={"lg"}
-      >
+      <Container sx={{ pt: 10, minHeight: "calc(100vh - 56px)", pb: 10 }} maxWidth={"lg"}>
         <Header />
+        <Loading />
         <Routes />
         <BottomBar />
       </Container>
