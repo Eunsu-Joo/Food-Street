e7d53b3586d5e077a4b0f30247d12a4e41a27652
Eunsu-Joo <holicholicpop@gmail.com>
2022-12-16 17:12:59 +0900

    ◾ 글로벌 style 프로퍼티 추가

--- src/style/style.ts
+++ src/style/style.ts
@@ -4,6 +4,15 @@ const theme = createTheme({
   typography: {
     fontFamily: ["Noto Sans KR", "Roboto", "Montserrat"].join(","),
   },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+        a{
+        text-decoration:initial;
+        color:inherit
+      `,
+    },
+  },
 });
 
 export default theme;
