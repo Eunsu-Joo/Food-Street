55eec15d5f4b4a105257bb36faec3afe9e4a8e8b
julie <julie@1024corp.com>
2023-06-30 15:50:00 +0900

    graphql error

--- client/src/graphql/fetcher.ts
+++ client/src/graphql/fetcher.ts
@@ -1,9 +1,9 @@
 import { request, RequestDocument } from "graphql-request";
-const BASE_URL =  process.env.REACT_APP_PRODUCTION_API_URL;
+const BASE_URL = "http://ec2-52-78-80-40.ap-northeast-2.compute.amazonaws.com:4000"
 
 const fetcher = (query: RequestDocument, data = {}) =>
   request(`${BASE_URL}/graphql`, query, data, {
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": BASE_URL as string
+    "Content-Type": "application/json", 
+
   });
 export default fetcher;
