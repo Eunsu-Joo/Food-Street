32546102a61a0707ceb62cd371e6e1c3b1c6fcea
julie <julie@1024corp.com>
2023-06-30 14:03:47 +0900

    baseURL, privite 수정

--- client/src/graphql/fetcher.ts
+++ client/src/graphql/fetcher.ts
@@ -1,8 +1,9 @@
 import { request, RequestDocument } from "graphql-request";
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = process.env.NODE_ENV === "development" ? process.env.REACT_APP_DEV_API_URL : process.env.REACT_APP_PRODUCTION_API_URL;
+console.log(process.env.REACT_APP_DEV_API_URL);
 const fetcher = (query: RequestDocument, data = {}) =>
   request(`${BASE_URL}/graphql`, query, data, {
     "Content-Type": "application/json",
-    "Access-Control-Allow-Origin": BASE_URL
+    "Access-Control-Allow-Origin": BASE_URL as string
   });
 export default fetcher;
